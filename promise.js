9b3f380dd8f74f9056d14a4dad58f53c502c5936
erwinleosinaga <erwindev.tech@gmail.com>
2022-09-20 00:30:18 +0700

    promise challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,43 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (data) => {
+  return new Promise((resolve, reject) => {
+    if (data === null) {
+      reject(`Terjadi kesalahan`);
+    } else {
+      promiseTheaterIXX().then((dataPengunjung) => {
+        const isiDatapengunjungIXX = [
+          dataPengunjung[0].hasil,
+          dataPengunjung[1].hasil,
+          dataPengunjung[2].hasil,
+        ];
+
+        promiseTheaterVGC().then((dataPengunjung) => {
+          const isiDatapengunjungVGC = [
+            dataPengunjung[0].hasil,
+            dataPengunjung[1].hasil,
+            dataPengunjung[2].hasil,
+          ];
+
+          let isiDataGabungan =
+            isiDatapengunjungIXX.concat(isiDatapengunjungVGC);
+
+          let itungIsiDataSama = {};
+          isiDataGabungan.forEach((jumlah) => {
+            itungIsiDataSama[jumlah] = (itungIsiDataSama[jumlah] || 0) + 1;
+          });
+
+          if (data === isiDataGabungan[0]) {
+            resolve(itungIsiDataSama.marah);
+          } else {
+            resolve(itungIsiDataSama.marah - 2);
+          }
+        });
+      });
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
