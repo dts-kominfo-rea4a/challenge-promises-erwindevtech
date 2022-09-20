27fcdc04ab411a5910aad78c561f13a9fb77a2f8
erwinleosinaga <erwindev.tech@gmail.com>
2022-09-20 08:58:07 +0700

    update new

--- promise.js
+++ promise.js
@@ -1,41 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = (data) => {
-  return new Promise((resolve, reject) => {
-    if (data === null) {
-      reject(`Terjadi kesalahan`);
-    } else {
-      promiseTheaterIXX().then((dataPengunjung) => {
-        const isiDatapengunjungIXX = [
-          dataPengunjung[0].hasil,
-          dataPengunjung[1].hasil,
-          dataPengunjung[2].hasil,
-        ];
-
-        promiseTheaterVGC().then((dataPengunjung) => {
-          const isiDatapengunjungVGC = [
-            dataPengunjung[0].hasil,
-            dataPengunjung[1].hasil,
-            dataPengunjung[2].hasil,
-          ];
-
-          let isiDataGabungan =
-            isiDatapengunjungIXX.concat(isiDatapengunjungVGC);
-
-          let itungIsiDataSama = {};
-          isiDataGabungan.forEach((jumlah) => {
-            itungIsiDataSama[jumlah] = (itungIsiDataSama[jumlah] || 0) + 1;
-          });
-
-          if (data === isiDataGabungan[0]) {
-            resolve(itungIsiDataSama.marah);
-          } else {
-            resolve(itungIsiDataSama.marah - 2);
-          }
-        });
-      });
+const promiseOutput = (emosi) => {
+  return new Promise(async (resolve, reject) => {
+    if (emosi == null || emosi.isEmpty) {
+      return reject("wrong input");
     }
+
+    let result = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()]);
+
+    return resolve(
+      result.reduce(
+        (total, current) =>
+          total + current.filter((obj) => obj.hasil == emosi).length,
+        0
+      )
+    );
   });
 };
 
